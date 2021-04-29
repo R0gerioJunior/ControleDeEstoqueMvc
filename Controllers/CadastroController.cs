434c130a89f1bc69c4293ece669199d46c523ea1
R0gerioJunior <rgrjunior2011@gmail.com>
2021-04-29 19:51:38 -0300

    ControleDeEstoqueMvc

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleDeEstoque.Controllers
+{
+    public class CadastroController : Controller
+    {        
+        public ActionResult GrupoProduto()
+        {
+            return View();
+        }
+
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+
+        public ActionResult LocalProduto()
+        {
+            return View();
+        }
+
+        public ActionResult UnidadeMedidaProduto()
+        {
+            return View();
+        }
+
+        public ActionResult Produto()
+        {
+            return View();
+        }
+
+        public ActionResult PaisProduto()
+        {
+            return View();
+        }
+
+        public ActionResult EstadoProduto()
+        {
+            return View();
+        }
+
+        public ActionResult CidadeProduto()
+        {
+            return View();
+        }
+
+        public ActionResult FornecedorProduto()
+        {
+            return View();
+        }
+
+        public ActionResult PerfilUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }        
+    }
+}
---
+++ Controllers/GraficoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleDeEstoque.Controllers
+{
+    public class GraficoController : Controller
+    {        
+        public ActionResult PerdasMes()
+        {
+            return View();
+        }
+
+        public ActionResult EstradaSaidaMes()
+        {
+            return View();
+        }
+    }
+}
